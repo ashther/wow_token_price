438fc87b03fbc19c48680b9505637f4d99ea6635
ashther <ashther@qq.com>
2021-03-12 16:40:11 +0800

    update data

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 source('crawler.R')
 source('update.R')
 
+Sys.setenv(RSTUDIO_PANDOC='/usr/lib/rstudio-server/bin/pandoc')
 rmarkdown::render('index.Rmd', output_dir = 'docs', quiet = FALSE)
 system('git add .')
 system("git commit -m 'update data'")
